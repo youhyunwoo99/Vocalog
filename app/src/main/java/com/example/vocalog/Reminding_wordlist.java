08d49260ba9ac37fb0dbb11dc4cef4ab3a87e910
Eunshik Kang <kes6091@gmail.com>
2023-05-26 03:48:41 +0900

    전공 meaning sepell test

--- app/src/main/java/com/example/vocalog/Reminding_wordlist.java
+++ app/src/main/java/com/example/vocalog/Reminding_wordlist.java
@@ -1,4 +1,25 @@
 package com.example.vocalog;
 
-public class Reminding_wordlist {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Reminding_wordlist extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminding_wordlist);
+
+        Intent intent = getIntent();
+        // Retrieve the boardTitle passed from the previous activity
+        String boardTitle = intent.getStringExtra("boardTitle");
+
+        TextView pageId = findViewById(R.id.page_id);
+        pageId.setText(boardTitle); // Set the boardTitle as the text of the TextView
+
+    }
+
 }
+
