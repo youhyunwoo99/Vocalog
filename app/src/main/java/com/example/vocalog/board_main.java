998715c4b68e2e27995e66bc4347c6865e9d5ab8
Haden Cho <yutmugaa100@gmail.com>
2023-05-25 16:44:43 +0900

    boardlist & createboard

---
+++ app/src/main/java/com/example/vocalog/board_main.java
@@ -0,0 +1,14 @@
+package com.example.vocalog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class board_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board_main);
+    }
+}
