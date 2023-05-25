725fbfea2f700b5d2edb80dc9c247f6971815f0e
Haden Cho <yutmugaa100@gmail.com>
2023-05-26 02:25:01 +0900

    "Memorizing End"

---
+++ app/src/main/java/com/example/vocalog/web_view.java
@@ -0,0 +1,20 @@
+package com.example.vocalog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class web_view extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        webView =findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://dict.naver.com/");
+    }
+}
