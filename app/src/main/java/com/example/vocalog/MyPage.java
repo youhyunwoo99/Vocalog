913423ac658e9aa5f1dde8a9b2f7f1b2cb8886c7
Eunshik Kang <kes6091@gmail.com>
2023-06-04 23:03:00 +0900

    프로필 사진 갤러리에서 가져와서 변경

--- app/src/main/java/com/example/vocalog/MyPage.java
+++ app/src/main/java/com/example/vocalog/MyPage.java
@@ -1,15 +1,32 @@
 package com.example.vocalog;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MyPage extends Fragment {
+
+    private final int REQUEST_PROFILE_PIC = 1;
+
+    private void startProfileActivity() {
+        Intent intent1 = new Intent(getActivity(), profile.class);
+        startActivityForResult(intent1, REQUEST_PROFILE_PIC);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,6 +37,8 @@ public class MyPage extends Fragment {
         CalendarView calendarView = rootView.findViewById(R.id.calendarView);
 
         // CalendarView의 이벤트 리스너를 설정합니다.
+
+
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
@@ -27,6 +46,7 @@ public class MyPage extends Fragment {
                 // 예를 들어, 선택한 날짜의 정보를 출력하거나 처리할 수 있습니다.
             }
         });
+
 
         // 버튼을 찾아옵니다.
         Button button = rootView.findViewById(R.id.button);
@@ -48,11 +68,29 @@ public class MyPage extends Fragment {
             public void onClick(View v) {
                 // 다른 페이지로 이동하는 코드를 작성합니다.
                 // 예를 들어, 다른 액티비티로 이동하는 코드를 작성할 수 있습니다.
-                Intent intent1 = new Intent(getActivity(), profile.class);
-                startActivity(intent1);
+
+                startProfileActivity();
             }
         });
 
         return rootView;
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_PROFILE_PIC) {
+            if (resultCode == RESULT_OK) {
+                Uri imageUri = data.getData();
+                // 이미지를 사용하여 필요한 작업을 수행합니다.
+                // 예를 들어, ImageView에 이미지를 설정할 수 있습니다.
+                ImageView myPageProfileImage = getView().findViewById(R.id.profileImageView);
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
+                    myPageProfileImage.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/vocalog/profile.java
+++ app/src/main/java/com/example/vocalog/profile.java
@@ -1,29 +1,76 @@
 package com.example.vocalog;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
+import android.widget.ImageView;
+import android.provider.MediaStore;
+import android.graphics.Bitmap;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.widget.TextView;
 
 public class profile extends AppCompatActivity {
+
+    private static final int SELECT_PROFILE_PIC = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
-        // 버튼을 찾아옵니다.
+// 버튼을 찾아옵니다.
         Button backButton = findViewById(R.id.backButton);
+        Button photoButton = findViewById(R.id.photo);
 
-        // 버튼의 클릭 이벤트 리스너를 설정합니다.
+
+
+
+// 버튼의 클릭 이벤트 리스너를 설정합니다.
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 액티비티를 종료하고 이전 화면으로 돌아갑니다.
+// 액티비티를 종료하고 이전 화면으로 돌아갑니다.
                 finish();
             }
         });
 
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickImage();
+            }
+        });
 
+        checkGalleryAccessPermission();
     }
+    private void pickImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, SELECT_PROFILE_PIC);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_PROFILE_PIC) {
+            if (resultCode == RESULT_OK) {
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        }
+    }
+
+    private void checkGalleryAccessPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+    }
+
+
 }
